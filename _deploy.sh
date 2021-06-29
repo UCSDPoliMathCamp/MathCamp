b8c6f7105a6898c5dd2853e8ffb040d63b43eed8
Keng-Chi Chang | 張耕齊 <kengchi.chang.tw@gmail.com>
2021-06-28 19:37:59 -0700

    switch from master to main branch

--- _deploy.sh
+++ _deploy.sh
@@ -7,7 +7,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "main" ] && exit 0
 
 git config --global user.email "kengchi.chang.tw@gmail.com"
 git config --global user.name "Keng-Chi Chang"
